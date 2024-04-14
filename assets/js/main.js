dbd9c5dfc738dd19eb1596d8f0c0dba5b5b007aa
chaitanyaj14 <chaitanyaj14@gmail.com>
2024-04-14 23:14:25 +0530

    minor spell edits

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
-  strings : ["Researcher", "Program Manager","GIS Developer"],
+  strings : ["Researcher", "Manager", "Developer"],
   loop : true,
   typeSpeed : 100, 
   backSpeed : 80,
